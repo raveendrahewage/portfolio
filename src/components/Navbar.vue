a6c8f3b098ff55982fd154759b5ba6f5ca941d10
raveendraHewage <hewagerv@gmail.com>
2021-05-02 08:45:43 +0530

    navbar issue fixed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,11 +3,11 @@
         <div class="max-width">
             <div class="logo"><a href="#">R<span>V.</span></a></div>
             <ul class="menu" id="menu">
-                <li><a href="#home" class="menu-btn">Home</a></li>
-                <li><a href="#about" class="menu-btn">About</a></li>
-                <li><a href="#skills" class="menu-btn">Skills</a></li>
-                <li><a href="#projects" class="menu-btn">Projects</a></li>
-                <li><a href="#contact" class="menu-btn">Contact</a></li>
+                <li><a href="#home" @click="menu" class="menu-btn">Home</a></li>
+                <li><a href="#about" @click="menu" class="menu-btn">About</a></li>
+                <li><a href="#skills" @click="menu" class="menu-btn">Skills</a></li>
+                <li><a href="#projects" @click="menu" class="menu-btn">Projects</a></li>
+                <li><a href="#contact" @click="menu" class="menu-btn">Contact</a></li>
             </ul>
             <div class="menu-btn" @click="menu">
                 <i class="fas fa-bars" id="menu-icon"></i>
